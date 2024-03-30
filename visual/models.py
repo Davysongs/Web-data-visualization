24b1137ae17b18a8e0a2d063a2a35a705407ab51
Davysongs <Davidgsongs@gmail.com>
2024-03-30 17:27:19 +0100

    Setup

--- visual/models.py
+++ visual/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class MarketInsight(models.Model):
+  """
+  Model to represent market insights data.
+  """
+  title = models.CharField(max_length=255)
+  sector = models.CharField(max_length=255)
+  topic = models.CharField(max_length=255)
+  insight = models.TextField()
+  url = models.URLField(blank=True)  # Allow empty URL
+  region = models.CharField(max_length=255)
+  country = models.CharField(max_length=255)
+  impact = models.CharField(max_length=255, blank=True)  # Allow empty impact
+  pestle = models.CharField(max_length=255, blank=True)  # Allow empty pestle
+  source = models.CharField(max_length=255)
+  likelihood = models.IntegerField()
+  relevance = models.IntegerField()
+  intensity = models.IntegerField()
+
+  # Dates with human-readable format
+  published = models.DateTimeField(blank=True)  # Allow empty published date
+  added = models.DateTimeField(blank=True)  # Allow empty added date
+
+  def __str__(self):
+    return self.title
